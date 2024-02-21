48b9843acdda0189bab3afdca9efe62c73bd2046
Sebastian <sbm.98.d@gmail.com>
2024-02-21 00:09:33 -0500

    add navegation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import Menu from "./components/Navbar";
+import { Container } from "@mui/material";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="" element={<TaskList />}/>
-      </Routes>
+    <Menu/>
+      <Container>
+        <Routes>
+          <Route path="" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+          {/* Edit route */}
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
